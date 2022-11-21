a4a82bdc989bfc876009ba425742f16dd814168c
kodar26x <kodardwirisman@gmail.com>
2022-11-21 15:36:16 +0700

    masuk mas

--- src/Components/Coffee/Coffee.jsx
+++ src/Components/Coffee/Coffee.jsx
@@ -6,74 +6,60 @@ import Navbar from "../Navbar/Navbar";
 function Coffee() {
     return (
         <div id="bg1">
-            <Navbar/>
+            <Navbar />
             <div className="container">
                 <div className="row" id="p1" >
-                    
-                        <div className="card col-6" style={{ width: "18rem",marginTop:"10px"}}>
-                            <img src={coba1} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h4>Nama Kopi Asal</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div className="col-8">
+                        <div className="row">
+                            <div className="card col-6" style={{ width: "18rem", marginTop: "10px" }}>
+                                <img src={coba1} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h4>Nama Kopi Asal</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card col-6" style={{ width: "18rem", marginTop: "10px" }}>
+                                <img src={coba2} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h4>Nama Kopi Asal</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card col-6" style={{ width: "18rem", marginTop: "10px" }}>
+                                <img src={coba1} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h4>Nama Kopi Asal</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card col-6" style={{ width: "18rem", marginTop: "10px" }}>
+                                <img src={coba2} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h4>Nama Kopi Asal</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card col-6" style={{ width: "18rem", marginTop: "10px" }}>
+                                <img src={coba1} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h4>Nama Kopi Asal</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card col-6" style={{ width: "18rem", marginTop: "10px" }}>
+                                <img src={coba2} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h4>Nama Kopi Asal</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
                             </div>
                         </div>
-                        <div className="card col-6" style={{ width: "18rem",marginTop:"10px" }}>
-                            <img src={coba2} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h4>Nama Kopi Asal</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                        <div className="card col-6" style={{ width: "18rem",marginTop:"10px"}}>
-                            <img src={coba1} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h4>Nama Kopi Asal</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                        <div className="card col-6" style={{ width: "18rem",marginTop:"10px" }}>
-                            <img src={coba2} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h4>Nama Kopi Asal</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                 
-                    
-                        <div className="card col-6" style={{ width: "18rem",marginTop:"10px"}}>
-                            <img src={coba1} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h4>Nama Kopi Asal</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                        <div className="card col-6" style={{ width: "18rem",marginTop:"10px" }}>
-                            <img src={coba2} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h4>Nama Kopi Asal</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                        <div className="card col-6" style={{ width: "18rem",marginTop:"10px"}}>
-                            <img src={coba1} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h4>Nama Kopi Asal</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                        <div className="card col-6" style={{ width: "18rem",marginTop:"10px" }}>
-                            <img src={coba2} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h4>Nama Kopi Asal</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                 
+                    </div>
+                    <div className="col-4">
+                        <img src={about} alt="" style={{ width: "50px", height: "50px" }} />
+                    </div>
                 </div>
-                        {/* <img src={about} alt="" style={{width:"50px",height:"50px",display:"flex",justifyContent:"end"}}/> */}
             </div>
-            
-
         </div>
     );
 }
