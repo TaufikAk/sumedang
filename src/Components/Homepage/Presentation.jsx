4575a6e73903b76a4009ec39a69b065f021ecd64
TaufikAk <akbartaufik9@gmail.com>
2022-11-23 19:01:51 +0700

    mantap bos

--- src/Components/Homepage/Presentation.jsx
+++ src/Components/Homepage/Presentation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Presentation() {
   return (
     <div id='presentation'>
-        <h1>Mari bergabung bersama!a</h1>
+        <h1>Mari bergabung bersama!</h1>
         <p>Gratis dan dapat diakses di mana saja!</p>
     </div>
   )
