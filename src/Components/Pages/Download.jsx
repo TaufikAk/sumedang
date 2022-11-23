c7705571c413e92e493f41e9b5ae64a880dc3846
TaufikAk <akbartaufik9@gmail.com>
2022-11-23 18:41:08 +0700

    check

--- src/Components/Pages/Download.jsx
+++ src/Components/Pages/Download.jsx
@@ -3,17 +3,17 @@ import Navbar from "../Navbar/Navbar";
 
 const Download = () =>{
     return(
-        <>
-        <div id="download-page flex-row">
+        <div className="download-page flex-row">
             <div className="col-6">
                 <h2>Download Sekarang dan Dapatkan Semuanya</h2>
-                <p></p>
+                <p>Informasi lengkap mengenai kopi, 
+      edukasi dari para ekspertise di bidang kopi, dan informasi kegiatan bertemakan kopi. Semuanya dapat Anda dapatkan dan akses secara 
+      mudah melalui Scoffee. Aplikasi edukasi karya anak negeri!</p>
             </div>
         <div className="col-6">
             <img src="" alt=""></img>
         </div>
         </div>
-        </>
     )
 }
 
